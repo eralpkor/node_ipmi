a98ea22a355459e5ba0d63075b9972b08107cc4b
Eralp Kor <eralp.ny@gmail.com>
2023-05-24 08:50:03 -0400

    Add test hours option

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
+import inquirer from "inquirer";
+const prompt = inquirer.createPromptModule();
 
 const config = () => {
   return inquirer
@@ -19,11 +21,35 @@ const config = () => {
         message: "What's your BMC password?",
       },
       {
-        name: "target_cycle",
+        name: "chiper",
         type: "input",
-        message: "How long do you want to run the test?",
-        default: "12",
+        message: "What's SUTs cipher #, eg. 17",
       },
+      {
+        name: "hours",
+        type: "input",
+        message: "How many hours test will run?",
+      },
+      {
+        name: "options",
+        message: "What type of power cycle would you like to do?",
+        type: "checkbox",
+        choices: ["on", "off", "cycle", "reset"],
+        validate: (options) => {
+          if (!options.length) {
+            return "Choose at least one of the above, use space to choose the option";
+          }
+
+          return true;
+        },
+      },
+      // Edit later for running test cycle
+      // {
+      //   name: "target_cycle",
+      //   type: "input",
+      //   message: "How long do you want to run the test?",
+      //   default: "12",
+      // },
     ])
     .then((answer) => {
       return answer;
@@ -31,19 +57,5 @@ const config = () => {
     .catch((err) => console.log(err));
 };
 
-// const config = require("readline").createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// readline.xcc_ip(`What's your XCC ip address? `, (ip) => {
-//   // console.log(`Hi ${name}!`);
-//   return ip;
-//   readline.close();
-// });
-
-// readline();
-
-// module.exports = { Config };
-module.exports = config;
-config();
+// module.exports = { config };
+export default config;
