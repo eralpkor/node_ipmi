b4b49b3f4b78dddef0a3081705d21d3d4590ba29
Eralp Kor <eralp.ny@gmail.com>
2023-11-20 13:38:42 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,12 +1,5 @@
 // tester fill SUT info,
 const setup = {
-  // bmcIp: "10.244.17.23",
-  // user: "USERID",
-  // pass: "Passw0rd123",
-  // cipher: "17",
-  // hours: "12",
-  // log_file: "charlotte_sit43",
-  // targetCount: 30,
   ipmiCommand: [
     ["0x00", "0x02", "0x00"],
     ["0x00", "0x02", "0x01"],
